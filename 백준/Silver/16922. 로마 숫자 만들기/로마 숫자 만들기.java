882922f5f4bad4b7101921ffc505c05af4b776df
SiHejt <91488469+SiHejt@users.noreply.github.com>
2025-06-24 12:58:00 +0900

    [Silver III] Title: 로마 숫자 만들기, Time: 104 ms, Memory: 14160 KB -BaekjoonHub

---
+++ 백준/Silver/16922. 로마 숫자 만들기/로마 숫자 만들기.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static boolean[] checkBool = new boolean[1001];
+    static int[] numbers = {1, 5, 10, 50};
+    static int ans;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+
+        backtracking(0, n, 0, 0);
+        System.out.println(ans);
+    }
+
+    static void backtracking(int sum, int n, int depth, int index) {
+
+        if (depth == n) {
+            if (!checkBool[sum]) {
+                checkBool[sum] = true;
+                ans++;
+            }
+            return;
+        }
+
+        for (int i = index; i < 4; i++) {
+            backtracking(sum + numbers[i], n, depth+1, i);
+        }
+    }
+}
