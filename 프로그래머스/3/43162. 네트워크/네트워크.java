85efc93e13a60f272881a2441e17a1a4631c20cb
SiHejt <91488469+SiHejt@users.noreply.github.com>
2025-09-08 16:50:24 +0900

    [level 3] Title: 네트워크, Time: 0.38 ms, Memory: 88.8 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,36 +1,35 @@
 import java.util.*;
 
 class Solution {
-    int answer = 0;
     
     public int solution(int n, int[][] computers) {
+        int answer = 0;
         boolean[] visited = new boolean[n];
+        Queue<Integer> q = new LinkedList<>();
         
-        for(int i = 0; i<n; i++) {
-            if(visited[i] == false) {
-                bfs(n, computers, visited, i);
-            } else {
+        for(int i=0; i<n; i++){
+            if(visited[i]) {
                 continue;
             }
-        }
-        return answer;
-    }
-    
-    public void bfs(int n, int[][] computers, boolean[] visited, int start) {
-        Queue<Integer> q = new LinkedList<>();
-        q.add(start);
-        visited[start] = true;
-        
-        while(!q.isEmpty()) {
-            int current = q.poll();
-            for(int i = 0; i<n; i++) {
-                if(computers[current][i] == 1 && visited[i] == false) {
-                    visited[i] = true;
-                    q.add(i);
-                } 
+            
+            q.offer(i);
+            visited[i] = true;
+            answer++;
+            
+            while(!q.isEmpty()){
+                int current = q.poll();
+                
+                for(int j=0; j<n; j++){
+                    if(!visited[j] && computers[current][j] == 1){
+                        q.offer(j);
+                        visited[j] = true;
+                    }
+                }
             }
+            
         }
-        answer++;
-        return;
+        
+        return answer;
+    
     }
 }
