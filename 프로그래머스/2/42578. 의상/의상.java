73fdd5040d0c33e830f02275ca5c5e572e570a1b
SiHejt <91488469+SiHejt@users.noreply.github.com>
2026-06-15 09:58:11 +0900

    [level 2] Title: 의상, Time: 0.09 ms, Memory: 85.6 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,19 +1,19 @@
 import java.util.*;
 
 class Solution {
-    int answer = 1;
     public int solution(String[][] clothes) {
         HashMap<String, Integer> hm = new HashMap<>();
         
-        for (String[] cloth : clothes) {
+        for(String cloth[] : clothes) {
             String type = cloth[1];
             hm.put(type, hm.getOrDefault(type, 0) + 1);
         }
         
         int answer = 1;
-        for (int cnt : hm.values()) {
-            answer *= (cnt + 1);
+        for(int i : hm.values()) {
+            answer *= i+1;
         }
+        
         return answer - 1;
     }
 }
